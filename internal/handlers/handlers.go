cb678cea268e2af31398155e5e8970fad3561232
Kubilay Humm <lieqb2@gmail.com>
2021-10-25 20:13:44 +0300

    init 06

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,142 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
+)
+
+/*
+
+- somebody connects to our webpage
+- we display the home page, with Home function
+- we automatically call ws endpoint to connect
+- once they connect we listen ws (ListenForWs) and start a goroutine
+- everything they sent to us will be sent to a WsChannel
+- then we broadcastToAll
+
+*/
+
+var wsChan = make(chan WsPayload)
+var clients = make(map[WebSocketConnection]string)
+
+var views = jet.NewSet(
+	jet.NewOSFileSystemLoader("./html"),
+	jet.InDevelopmentMode(),
+)
+
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// wrapper
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+// WsJsonResponse defines the response sent back from websocket
+type WsJsonResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
+}
+
+// WsEndpoint upgrades connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("client connected to endpoint")
+	var response WsJsonResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
+
+	if err := ws.WriteJSON(response); err != nil {
+		log.Println(err)
+	}
+
+	go ListenForWs(&conn)
+}
+
+func ListenForWs(conn *WebSocketConnection) {
+	// when this func stops executing
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	// this is the type we will sent back to the user
+	var response WsJsonResponse
+
+	for {
+		e := <-wsChan
+
+		response.Action = "Got here"
+		response.Message = fmt.Sprintf("Some message, and action was %s", e.Action)
+		broadcastToAll(response)
+	}
+}
+
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket err") // generally means someone left the chat
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if err := renderPage(w, "home.jet", nil); err != nil {
+		log.Println(err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
